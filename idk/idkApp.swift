638260ee6b7c6a3440867195acd92aa99bd9dbe5
zaid ragab <zragab@lcs.on.ca>
2023-01-10 08:54:01 -0500

    Initial Commit

---
+++ idk/idkApp.swift
@@ -0,0 +1,17 @@
+//
+//  idkApp.swift
+//  idk
+//
+//  Created by Zaid Ragab on 2023-01-10.
+//
+
+import SwiftUI
+
+@main
+struct idkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
